ba7787d09ff48b973b6ce2d10503384820782dfd
jonathanhoskin <github@hosk.in>
2016-05-26 12:37:23 +1200

    WIP

--- lib/elbas/launch_configuration.rb
+++ lib/elbas/launch_configuration.rb
@@ -37,63 +37,58 @@ module Elbas
 
     private
 
-      def base_launch_config
-        launch_configs = autoscaling.client.describe_launch_configurations(
-          launch_configuration_names: [autoscale_group.launch_configuration_name]
-        )
-        puts launch_configs[:launch_configurations].first
-        abort
+    def base_launch_config
+      launch_configs = autoscaling.client.describe_launch_configurations(
+        launch_configuration_names: [autoscale_group.launch_configuration_name]
+      )
+      launch_configs[:launch_configurations].first
+    end
+
+    def launch_config_base_name
+      "elbas-#{environment}-#{autoscale_group_name}"
+    end
+
+    def name
+      @name ||= timestamp(launch_config_base_name)
+    end
+
+    def instance_type
+      fetch(:aws_autoscale_instance_type, base_launch_config[:instance_type])
+    end
+
+    def create_options
+      options = {
+        launch_configuration_name: name,
+        image_id: image_id,
+        instance_type: instance_type,
+        associate_public_ip_address: base_launch_config[:associate_public_ip_address],
+        ebs_optimized: base_launch_config[:ebs_optimized],
+        security_groups: base_launch_config[:security_groups],
+        instance_monitoring: base_launch_config[:instance_monitoring]
+      }
+
+      options[:block_device_mappings] = base_launch_config[:block_device_mappings] if base_launch_config[:block_device_mappings].present?
+      options[:iam_instance_profile] = base_launch_config[:iam_instance_profile] if base_launch_config[:iam_instance_profile].present?
+      options[:key_name] = base_launch_config[:key_name] if base_launch_config[:key_name].present?
+      options[:spot_price] = base_launch_config[:spot_price] if base_launch_config[:spot_price].present?
+      options[:user_data] = base_launch_config[:user_data] if base_launch_config[:user_data].present?
+
+      info "Launch configuration options: #{options}"
+
+      abort
+
+      options
+    end
+
+    def deployed_with_elbas?(lc)
+      lc.name.include? launch_config_base_name
+    end
+
+    def trash
+      autoscaling.launch_configurations.to_a.select do |lc|
+        deployed_with_elbas? lc
       end
+    end
 
-      def base_security_group_ids
-        security_groups = base_launch_config.security_groups.map { |sg| sg.security_group_id }
-        security_groups.length == 1 ? security_groups.first : security_groups
-      end
-
-      def launch_config_base_name
-        "elbas-#{environment}-#{autoscale_group_name}"
-      end
-
-      def name
-        @name ||= timestamp(launch_config_base_name)
-      end
-
-      def instance_type
-        fetch(:aws_autoscale_instance_type, base_launch_config.instance_type)
-      end
-
-      def create_options
-        options = {
-          launch_configuration_name: name,
-          image_id: image_id,
-          instance_type: instance_type,
-          associate_public_ip_address: base_launch_config.associate_public_ip_address,
-          ebs_optimized: base_launch_config.ebs_optimized,
-          security_groups: base_security_group_ids,
-          instance_monitoring: {
-            enabled: base_launch_config.detailed_instance_monitoring
-          }
-        }
-
-        options[:block_device_mappings] = base_launch_config.block_device_mappings if base_launch_config.block_device_mappings.present?
-        options[:iam_instance_profile] = base_launch_config.iam_instance_profile if base_launch_config.iam_instance_profile.present?
-        options[:key_name] = base_launch_config.key_name if base_launch_config.key_name.present?
-        options[:spot_price] = base_launch_config.spot_price if base_launch_config.spot_price.present?
-        options[:user_data] = base_launch_config.user_data if base_launch_config.user_data.present?
-
-        info "Launch configuration options: #{options}"
-
-        options
-      end
-
-      def deployed_with_elbas?(lc)
-        lc.name.include? launch_config_base_name
-      end
-
-      def trash
-        autoscaling.launch_configurations.to_a.select do |lc|
-          deployed_with_elbas? lc
-        end
-      end
   end
 end
