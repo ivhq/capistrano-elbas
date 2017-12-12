c8119638128a6de7d53be05801f05b0835785e9a
jonathanhoskin <github@hosk.in>
2017-12-12 21:53:33 +1300

    Add support for overriding AWS profile name

--- lib/elbas/aws/credentials.rb
+++ lib/elbas/aws/credentials.rb
@@ -6,14 +6,19 @@ module Elbas
 
       def credentials
         @_credentials ||= begin
-          _credentials = {}
+          if fetch(:aws_profile_name)
+            provider = ::AWS::Core::CredentialProviders::SharedCredentialFileProvider.new(profile_name: fetch(:aws_profile_name))
+            _credentials = { credential_provider: provider }
+          else
+            _credentials = {}
 
-          if fetch(:aws_access_key_id, ENV['AWS_ACCESS_KEY_ID']).present?
-            _credentials.merge! access_key_id: fetch(:aws_access_key_id, ENV['AWS_ACCESS_KEY_ID'])
-          end
+            if fetch(:aws_access_key_id, ENV['AWS_ACCESS_KEY_ID']).present?
+              _credentials.merge! access_key_id: fetch(:aws_access_key_id, ENV['AWS_ACCESS_KEY_ID'])
+            end
 
-          if fetch(:aws_secret_access_key, ENV['AWS_SECRET_ACCESS_KEY']).present?
-            _credentials.merge! secret_access_key: fetch(:aws_secret_access_key, ENV['AWS_SECRET_ACCESS_KEY'])
+            if fetch(:aws_secret_access_key, ENV['AWS_SECRET_ACCESS_KEY']).present?
+              _credentials.merge! secret_access_key: fetch(:aws_secret_access_key, ENV['AWS_SECRET_ACCESS_KEY'])
+            end
           end
 
           _credentials.merge! region: fetch(:aws_region) if fetch(:aws_region)
